a4c759eb158923d0cc1d2c714b9f696abc15827d
marlo <narl0durn0@ya.ru>
2022-06-24 23:08:03 +0300

    created user get serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,39 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-from recipes.models import (Cart, Favorite, Ingridient, IngridientInRecipe,
-                            Recipe, Tag, TagInRecipe)
+from users.models import User
+
+
+class UserGetSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+        )
+        model = User
+
+    def get_is_subscibed(self, obj: User):
+        request = self.context.get('request')
+        if request is None:
+            return False
+        return User.objects.filter(
+            user=request.user,
+            author__id=obj.id,
+        ).exists()
+
+
+class UserCreationSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+        )
+        model = User
