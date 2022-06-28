a3fc474192421cea0a62ba480de3606b756c66d8
marlo <narl0durn0@ya.ru>
2022-06-28 04:57:26 +0300

    sdf

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,12 +1,17 @@
+from django.core.handlers.exception import response_for_exception
+from django.http.response import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import SAFE_METHODS
+from rest_framework import status
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.decorators import action
 
 from .filters import IngredientFilter, RecipeFilter
-from .models import Ingredient, Recipe
+from .models import Cart, Ingredient, Recipe
 from .permissions import AuthorOrReadOnly
 from .serializers import (IngredientSerializer, RecipeCreateSerializer,
-                          RecipeGetSerializer)
+                          RecipeGetSerializer, RecipeShortInfo)
 
 
 class IngredientsViewSet(ReadOnlyModelViewSet):
@@ -27,3 +32,62 @@ class RecipeViewSet(ModelViewSet):
         if self.request.method not in SAFE_METHODS:
             return RecipeCreateSerializer
         return RecipeGetSerializer
+
+    @action(
+        path='shopping_cart',
+        detail=True,
+        methods=('post', 'delete'),
+        permission_classes=(IsAuthenticated,),
+    )
+    def shopping_cart(self, request, id=None):
+        if id is None or not Recipe.objects.filter(id=id).exists():
+            return Response(
+                {'error': 'sdfsdf'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        recipe = Recipe.objects.get(id=id)
+        Cart.objects.create(
+            user=request.user,
+            recipe=recipe,
+        )
+        return Response(
+            RecipeShortInfo(recipe, many=False),
+            status=status.HTTP_201_CREATED,
+        )
+
+    @action(
+        path='download_shopping_cart',
+        detail=False,
+        methods=('get',),
+        permission_classes=(IsAuthenticated,),
+    )
+    def download_shopping_cart(self, request):
+        recipes = request.user.recipes.all()
+        shopping_list = {}
+        for ingredient in recipes.ingredients.all():
+            name = ingredient.ingredient.name
+            amount = ingredient.amount
+            measurement_unit = ingredient.measurement_unit
+            if name in shopping_list:
+                shopping_list[name]['amount'] += amount
+            else:
+                shopping_list[name] = {
+                    'amount': amount,
+                    'measurement_unit': measurement_unit,
+                }
+        cart = [
+            '%(name)s %(amount)s %(measurement_unit)s\n' % {
+                'name': name,
+                'amount': data['amount'],
+                'measurement_unit': data['measurement_unit'],
+            } for name, data in shopping_list.items()
+        ]
+        response = HttpResponse(
+            cart,
+            'Content-Type: text/plain',
+        )
+        response['Content-Disposition'] = (
+            'attachment; filename="shopping_cart.txt"'
+        )
+        return response
+
