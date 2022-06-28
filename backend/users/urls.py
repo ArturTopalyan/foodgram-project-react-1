689e8a8c1b3e33135e820b815aa9640eb1b12142
marlo <narl0durn0@ya.ru>
2022-06-29 02:26:20 +0300

    sl

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import FollowUnfollowUser
 
 urlpatterns = [
-    path('users/<int:id>/subscribe/', FollowUnfollowUser.as_view()),
+    path('users/<str:id>/subscribe/', FollowUnfollowUser.as_view()),
 ]
